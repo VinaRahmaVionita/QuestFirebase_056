d864cd93aca2aea4a14953d66c9d46b6203dffe6
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-11 18:46:59 +0700

    menghapus data mahasiswa

--- app/src/main/java/com/example/project9/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/project9/repository/NetworkMahasiswaRepository.kt
@@ -49,5 +49,16 @@ class NetworkMahasiswaRepository(
     }
 
 
+    override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa:${e.message}")
+        }
+    }
+
 
 }

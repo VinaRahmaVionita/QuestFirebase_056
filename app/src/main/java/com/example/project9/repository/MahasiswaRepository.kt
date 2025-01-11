9c120be93f401855445baad9c2a63b7d3e1c2f67
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-11 18:43:14 +0700

    menyesuaikan parameter delete dan update mahasiswa

--- app/src/main/java/com/example/project9/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/project9/repository/MahasiswaRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-    suspend fun updateMahasiswa(nim:String, mahasiswa: Mahasiswa)
-    suspend fun deleteMahasiswa(nim: String)
     suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }

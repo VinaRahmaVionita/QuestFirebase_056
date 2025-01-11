3020423782c1f08ad5df731a5419820e7b519fb6
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-01-11 18:31:42 +0700

    menyimpan input form kedalam entity

---
+++ app/src/main/java/com/example/project9/ui/theme/viewmodel/InsertViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.project9.ui.theme.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project9.model.Mahasiswa
+import com.example.project9.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+
+//menyimpan input form kedalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenis_kelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
